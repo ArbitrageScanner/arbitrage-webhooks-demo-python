7a1de0b11bcffd6531e4bae1bd35702c4f1d6d8c
Arbitrage Scanner <info@arbitragescanner.io>
2025-08-19 22:18:05 +0300

    Feat: initial commit

---
+++ models.py
@@ -0,0 +1,79 @@
+from pydantic import BaseModel, ConfigDict, Field
+from pygments.lexer import default
+
+
+class Headers(BaseModel):
+    host: str
+    user_agent: str = Field(alias="user-agent")
+    content_encoding: str = Field(alias="content-encoding")
+    content_type: str = Field(alias="content-type")
+    traceparent: str
+    hook_token: str = Field(alias="x-hook-token")
+    spread_batch_id: str = Field(alias="x-spread-batch-id")
+    accept_encoding: str = Field(alias="accept-encoding")
+
+    model_config = ConfigDict(
+        extra="ignore",
+        populate_by_name=True
+    )
+
+class Chain(BaseModel):
+    chain: str
+    deposit_enabled: bool = Field(alias='deposit_enabled')
+    withdraw_enabled: bool = Field(alias='withdraw_enabled')
+    withdraw_fee: float = Field(alias='withdrawFee')
+    min_confirm: int = Field(alias="minConfirm")
+
+    model_config = ConfigDict(
+        extra="ignore",
+        populate_by_name=True
+    )
+
+class Spread(BaseModel):
+    profit_index_max: float = Field(alias='profitIndexMax')
+    profit_index_min: float = Field(alias='profitIndexMin')
+    profit_index_avg: float = Field(alias='profitIndexAvg')
+    volume: float
+    buy_price_min: float = Field(alias='buyPriceMin')
+    buy_price_max: float = Field(alias='buyPriceMax')
+    buy_price_avg: float = Field(alias='buyPriceAvg')
+    sell_price_min: float = Field(alias='sellPriceMin')
+    sell_price_max: float = Field(alias='sellPriceMax')
+    sell_price_avg: float = Field(alias='sellPriceAvg')
+    exchange_buy: str = Field(alias='exchangeBuy')
+    exchange_sell: str = Field(alias='exchangeSell')
+    symbol: str
+    buy_exchange_funding_rate: float = Field(alias='buyExchangeFundingProfitModifier', default=0)
+    sell_exchange_funding_rate: float = Field(alias='sellExchangeFundingProfitModifier', default=0)
+    buy_exchange_next_funding_apply_time: int = Field(alias='buyExchangeNextFundingTime', default=0)
+    sell_exchange_next_funding_apply_time: int = Field(alias='sellExchangeNextFundingTime', default=0)
+    overall_profit_index_max: float = Field(alias='overallProfitIndexMax')
+    overall_profit_index_min: float = Field(alias='overallProfitIndexMin')
+    overall_profit_index_avg: float = Field(alias='overallProfitIndexAvg')
+    original_symbol: str = Field(alias='originalSymbol')
+    volume_usd: float = Field(alias='volumeUsd')
+    lifetime: int
+    chains_buy: list[Chain] = Field(alias='chainsBuy', default=[])
+    chains_sell: list[Chain] = Field(alias='chainsSell', default=[])
+    updated: int
+    is_futures: bool = Field(alias='isFutures')
+
+    model_config = ConfigDict(
+        extra="ignore",
+        populate_by_name=True
+    )
+
+class HookRecord(BaseModel):
+    hook_token: str
+    receive_date: int #in milliseconds
+    receive_date_str: str
+    headers: Headers
+    received_body: list[Spread]
+    response_date: float | None = Field(default=None)
+    response_body: dict
+    error: str | None = Field(default=None)
+
+    model_config = ConfigDict(
+        extra="ignore",
+        populate_by_name=True
+    )
---
+++ server.py
@@ -0,0 +1,161 @@
+import asyncio
+import datetime
+import gzip
+import json
+import logging
+import os
+import time
+
+from io import BytesIO
+    from logging.handlers import TimedRotatingFileHandler
+
+from fastapi import FastAPI, Request, status, APIRouter
+from fastapi.responses import JSONResponse
+
+import uvicorn as uvicorn
+
+from models import HookRecord, Spread, Headers
+
+SERVER_IP = os.getenv('SERVER_IP')
+SERVER_PORT = os.getenv('SERVER_PORT', 8181)
+
+os.makedirs("logs", exist_ok=True)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S %Z"
+)
+
+# This handler will save app logs directly in file in project folder
+file_handler = TimedRotatingFileHandler(
+    "logs/app.log",
+    when="D",
+    interval=1,
+    backupCount=7,
+    encoding="utf-8"
+)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+# This is made to have logs in docker as well (as stdout).
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+logger.addHandler(console_handler)
+
+app = FastAPI()
+router = APIRouter()
+
+class HooksAcceptor:
+    def __init__(self):
+        self.hooks: dict[str, list[HookRecord]] = {}
+        self.lock = asyncio.Lock()
+
+    async def unpack_gzip(self, raw_body: bytes) -> dict | list:
+        with gzip.GzipFile(fileobj=BytesIO(raw_body)) as f:
+            decompressed_data = f.read()
+            decoded_json = json.loads(decompressed_data)
+        return decoded_json
+
+    async def accept_hook(self, request: Request, status_code=status.HTTP_200_OK) -> JSONResponse:
+
+        logger.info('Received hook')
+
+        receive_date = int(time.time()*1000)
+        receive_date_str = datetime.datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S %Z")
+
+        body = await request.body()
+
+        headers_dict = {key: request.headers[key] for key in request.headers}
+
+        error = None
+
+        success = True
+
+        try:
+
+            decoded_body = await self.unpack_gzip(raw_body=body)
+            received_body = [Spread(**spread) for spread in decoded_body.get('data', [])]
+            response_date = time.time()
+
+            logger.info('Successfully decoded json.')
+
+        except Exception as err:
+
+            response_date = None
+            received_body = []
+            error = str(err)
+            success = False
+
+            logger.error(f'An error occured: {err}.')
+
+        finally:
+            response_body = {
+                "success": success,
+                "status_code": status_code
+            }
+
+        hook_token = headers_dict.get('hook_token')
+
+        if not self.hooks.get(hook_token):
+            self.hooks[hook_token] = []
+
+        self.hooks[hook_token].append(
+            HookRecord(
+                hook_token=hook_token,
+                receive_date=receive_date,
+                receive_date_str=receive_date_str,
+                received_body=received_body,
+                headers=Headers(**headers_dict),
+                response_date=response_date,
+                response_body=response_body,
+                error=error
+            )
+        )
+
+        logger.info('Saved spreads from hook in RAM. Access via /stats.')
+
+        # For the sake of saving your RAM - we will save only last 10 hooks.
+        # If you want to keep all hooks, that are received by server - just delete next two lines.
+        async with self.lock:
+            self.hooks[hook_token] = self.hooks[hook_token][-10:]
+
+        logger.info('Returning response to hook sender.')
+        return JSONResponse(
+            status_code=status_code,
+            headers={},
+            content=response_body
+        )
+
+    async def stats(self, status_code=status.HTTP_200_OK, hook_token: str = None) -> JSONResponse:
+
+        response_body = {
+            "result_status": "success",
+            "data": {}
+        }
+
+        # If hook_token is specified - only that hook spread batches will be shown in response
+        if hook_token:
+            response_body['data'][hook_token] = [record.model_dump() for record in self.hooks.get(hook_token, [])]
+
+        # Otherwise - all hooks currently in RAM will be shown
+        else:
+            for hook in self.hooks:
+                response_body['data'][hook] = [record.model_dump() for record in self.hooks.get(hook, [])]
+
+        return JSONResponse(
+            status_code=status_code, headers={}, content=response_body
+        )
+
+
+handler = HooksAcceptor()
+router.post("/hook")(handler.accept_hook)
+router.get("/stats")(handler.stats)
+router.get("/stats/{hook_token}")(handler.stats)
+
+app.include_router(router)
+
+if __name__ == '__main__':
+    uvicorn.run("server:app", host=SERVER_IP, port=SERVER_PORT, reload=False)
